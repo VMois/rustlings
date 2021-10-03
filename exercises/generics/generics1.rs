b16cb9fcd5f02663c00981b72648bc1890741e56
VMois <vmois@protonmail.com>
2021-10-03 12:50:49 +0200

    finish generics

--- exercises/generics/generics1.rs
+++ exercises/generics/generics1.rs
@@ -6,6 +6,9 @@
 // I AM NOT DONE
 
 fn main() {
-    let mut shopping_list: Vec<?> = Vec::new();
+    // let mut shopping_list: Vec<String> = Vec::new();
+    // shopping_list.push("milk".to_string());
+
+    let mut shopping_list: Vec<&str> = Vec::new();
     shopping_list.push("milk");
 }
